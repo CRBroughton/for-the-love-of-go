003f358d1b1eda370963be4d811cc09a2a851b42
CRBroughton <crbroughton@posteo.uk>
2023-06-07 19:29:39 +0100

    feat: :sparkles: complete chapter II

--- 01-testing-times/calculator/calculator_test.go
+++ 01-testing-times/calculator/calculator_test.go
@@ -29,7 +29,7 @@ func TestAdd(t *testing.T) {
 	}
 }
 
-func TestSubtrat(t *testing.T) {
+func TestSubtract(t *testing.T) {
 	t.Parallel()
 
 	testCases := []testCase{
@@ -59,6 +59,29 @@ func TestMultiply(t *testing.T) {
 
 	for _, testCase := range testCases {
 		got := calculator.Multiply(testCase.a, testCase.b)
+
+		if testCase.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f",
+				testCase.a, testCase.b, testCase.want, got)
+		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+
+	testCases := []testCase{
+		{a: 2, b: 2, want: 1},
+		{a: -1, b: -1, want: 1},
+		{a: 10, b: 2, want: 5},
+	}
+
+	for _, testCase := range testCases {
+		got, err := calculator.Divide(testCase.a, testCase.b)
+
+		if err != nil {
+			t.Fatalf("want no error for valid input, got %v", err)
+		}
 
 		if testCase.want != got {
 			t.Errorf("Add(%f, %f): want %f, got %f",
