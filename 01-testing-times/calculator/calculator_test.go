0289eda92e87e677a8a3e7da725974d7f85762f7
CRBroughton <crbroughton@posteo.uk>
2023-06-08 19:15:34 +0100

    feat: :sparkles: complete division function

--- 01-testing-times/calculator/calculator_test.go
+++ 01-testing-times/calculator/calculator_test.go
@@ -2,12 +2,17 @@ package calculator_test
 
 import (
 	"calculator"
+	"math"
 	"testing"
 )
 
 type testCase struct {
 	a, b float64
 	want float64
+}
+
+func closeEnough(a, b, tolerance float64) bool {
+	return math.Abs(a-b) <= tolerance
 }
 
 func TestAdd(t *testing.T) {
@@ -74,6 +79,7 @@ func TestDivide(t *testing.T) {
 		{a: 2, b: 2, want: 1},
 		{a: -1, b: -1, want: 1},
 		{a: 10, b: 2, want: 5},
+		{a: 1, b: 3, want: 0.333333},
 	}
 
 	for _, testCase := range testCases {
@@ -83,9 +89,19 @@ func TestDivide(t *testing.T) {
 			t.Fatalf("want no error for valid input, got %v", err)
 		}
 
-		if testCase.want != got {
+		if !closeEnough(testCase.want, got, 0.001) {
 			t.Errorf("Add(%f, %f): want %f, got %f",
 				testCase.a, testCase.b, testCase.want, got)
 		}
+	}
+}
+
+func TestDivideInvalid(t *testing.T) {
+	t.Parallel()
+
+	_, err := calculator.Divide(1, 0)
+
+	if err == nil {
+		t.Error("want error for invalid input, got nil")
 	}
 }
