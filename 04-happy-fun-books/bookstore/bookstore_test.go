20a44724a46891b1b6b146bf17f3fe9b008ee9c3
CRBroughton <crbroughton@posteo.uk>
2023-06-10 14:47:52 +0100

    feat: :sparkles: complete chapter V

--- 04-happy-fun-books/bookstore/bookstore_test.go
+++ 04-happy-fun-books/bookstore/bookstore_test.go
@@ -13,3 +13,44 @@ func TestBook(t *testing.T) {
 		Copies: 2,
 	}
 }
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+
+	b := bookstore.Book{
+		Title:  "My First Book",
+		Author: "Craig R Broughton",
+		Copies: 2,
+	}
+
+	want := 1
+
+	result, err := bookstore.Buy(b)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got := result.Copies
+
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	t.Parallel()
+
+	b := bookstore.Book{
+		Title:  "My First Book",
+		Author: "Craig R Broughton",
+		Copies: 0,
+	}
+
+	_, err := bookstore.Buy(b)
+
+	if err == nil {
+		t.Error("want error buying from zero copies, got nils")
+	}
+}
